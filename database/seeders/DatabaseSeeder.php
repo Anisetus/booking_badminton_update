58a5c84623e5e4c25bf58d8c9f9023183f1860a8
Anisetus  Bambang Manalu <145880996+Anisetus@users.noreply.github.com>
2024-06-07 12:17:01 +0700

    uas_prak_done

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,97 +2,17 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-
-use App\Enums\UserRolesEnum;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
+     *
+     * @return void
      */
-    public function run(): void
+    public function run()
     {
-
-        // run location seeder
-        $this->call([
-            LocationSeeder::class,
-        ]);
-
-        $userroles = [
-            [
-                'id' => UserRolesEnum::Customer,
-                'name' => 'Customer',
-                'status' => true,
-            ],
-            [
-                'id' => UserRolesEnum::Employee,
-                'name' => 'Employee',
-                'status' => true,
-            ],
-            [
-                'id' => UserRolesEnum::Admin,
-                'name' => 'Admin',
-                'status' => true,
-            ]
-
-        ];
-
-        foreach ($userroles as $role) {
-            \App\Models\Role::create($role);
-        }
-
-        // Create admin user
-        \App\Models\User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('admin'),
-            'phone_number' => '1234569990',
-            'role_id' => UserRolesEnum::Admin,
-        ]);
-
-        // create mock customers
-        \App\Models\User::create([
-            'name' => '',
-            'email' => 'anisetusm@gmail.com',
-            'password' => Hash::make('anisetus'),
-            'phone_number' => '082276735592',
-            'role_id' => UserRolesEnum::Customer,
-        ]);
-
-        // create mock employees
-        \App\Models\User::create([
-            'name' => 'manalu',
-            'email' => 'manalu@gmail.com',
-            'password' => Hash::make('manalu'),
-            'phone_number' => '1234567899',
-            'role_id' => UserRolesEnum::Employee,
-        ]);
-
-        // categories Skin, Makeup, Nails, Hair
-        \App\Models\Category::create([
-            'name' => 'lapangan1',
-        ]);
-
-        \App\Models\Category::create([
-            'name' => 'lapangan2',
-        ]);
-
-        \App\Models\Category::create([
-            'name' => 'lapangan3',
-        ]);
-
-        \App\Models\Category::create([
-            'name' => 'lapangan4',
-        ]);
-
-        $this->call([
-            ServicesSeeder::class,
-            TimeSlotSeeder::class,
-        ]);
-
-
+        // \App\Models\User::factory(10)->create();
     }
 }
--- resources/views/components/application-mark.blade.php
+++
@@ -1,4 +0,0 @@
-<div class="inline-flex items-center">
-  <img src="{{ asset('images/del.png') }}" alt="" class="h-16 w-16">
-  <h1 class="text-3xl font-bold text-pink-500">DEL Badminton</h1>
-</div>
